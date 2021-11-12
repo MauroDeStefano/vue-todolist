7c697518827ffe9be1a94c8986e5d359e81501d6
MauroDeStefano <maurodestefano@gmail.com>
2021-11-12 15:49:40 +0100

    added milestone 2 and milestone 3

---
+++ js/milestone2.js
@@ -0,0 +1,86 @@
+// Esercizio di oggi: **Vue To Do List**
+// nome repo: vue-todolist
+// **Descrizione:**
+// Rifare l’esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - `text`, una stringa che indica il testo del todo
+// - `done`, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// **MILESTONE 1**
+// Stampare all’interno di una lista, un item per ogni todo.
+// Se la proprietà `done` è uguale a `true`, visualizzare il testo del todo ~~sbarrato~~.
+// **MILESTONE 2**
+// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+// **MILESTONE 3**
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// **Bonus:**
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se `done` era uguale a `false`, impostare `true` e viceversa)
+// Buon lavoro e buon divertimento!
+// :segno_spunta_bianco:
+
+
+const app = new Vue({
+  
+  el: "#app",
+
+  data: {
+    todoList:[
+      {
+        phrase: "studiare",
+        cutted: false 
+      },
+      {
+        phrase: "studiare",
+        cutted: false 
+      },
+      {
+        phrase: "studiare",
+        cutted: false 
+      },
+      {
+        phrase: "studiare",
+        cutted: false 
+      },
+      {
+        phrase: "studiare",
+        cutted: false 
+      },
+      {
+        phrase: "studiare",
+        cutted: false 
+      },
+    ],
+
+    error: false,
+
+    stringToDo: {
+      phrase: "",
+      cutted: false
+    }
+    },
+
+    methods:{
+
+      pushToDo(){
+        if(this.stringToDo.length < 3){
+          this.error = true;
+  
+        }else{
+          this.todoList.push(this.stringToDo);
+          this.stringToDo = {
+            phrase: "",
+            cutted: false
+          } 
+          this.error= false;
+          console.log(this.todoList);
+        }
+      },
+  
+      removeToDo(index){
+        this.todoList.splice(index, 1);
+      }
+    }
+
+});
+  
+
